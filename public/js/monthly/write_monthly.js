4eec3e9b906e62fecdc44f95ae69aeec50c09702
sy <greyhairchooselife@gmail.com>
2021-10-21 21:39:12 +0900

    add_more 버튼들 레이아웃 재조정, 연결된 js 수정

--- public/js/monthly/write_monthly.js
+++ public/js/monthly/write_monthly.js
@@ -22,72 +22,40 @@ let tag_maker;
 
 add_more_new.addEventListener('click', () => {
 	type_of_paragraph = 'addtion';
-	if(number_of_paragraph < 8){
-		tag_maker = document.createElement('input');
-		tag_maker.setAttribute("class", "paragraph_title");
-		tag_maker.setAttribute("id", "paragraph_title_"+number_of_paragraph);
-		tag_maker.setAttribute("type", "text");
-		tag_maker.setAttribute("name", "titles");
-		tag_maker.setAttribute("placeholder", "제목을 입력하십시오.");
-		writing_board.appendChild(tag_maker);
+	tag_maker = document.createElement('textarea');
+	tag_maker.setAttribute("class", "new_paragraph_coment");
+	tag_maker.setAttribute("id", "new_paragraph_coment_"+number_of_paragraph);
+	tag_maker.setAttribute("onkeydown", "resize(this)");
+	tag_maker.setAttribute("name", "coments");
+	tag_maker.setAttribute("placeholder", "떠오르는 생각들, 맴도는 생각들을 자유롭게 써 보세요.");
+	writing_board.appendChild(tag_maker);
 
-		tag_maker = document.createElement('textarea');
-		tag_maker.setAttribute("class", "paragraph_content");
-		tag_maker.setAttribute("id", "paragraph_content_"+number_of_paragraph);
-		tag_maker.setAttribute("name", "contents");
-		tag_maker.setAttribute("placeholder", "내용을 입력하세요.");
-		tag_maker.setAttribute("cols", "68");
-		writing_board.appendChild(tag_maker);
+	tag_maker = document.createElement('input');
+	tag_maker.setAttribute("type", "hidden");
+	tag_maker.setAttribute("name", "titles");
+	tag_maker.setAttribute("value", "");
+	writing_board.appendChild(tag_maker);
 
+	tag_maker = document.createElement('input');
+	tag_maker.setAttribute("type", "hidden");
+	tag_maker.setAttribute("name", "contents");
+	tag_maker.setAttribute("value", "");
+	writing_board.appendChild(tag_maker);
 
-		document.getElementById('paragraph_title_'+number_of_paragraph).focus();
-//		document.getElementById('paragraph_title_'+number_of_paragraph).addEventListener('click', () => {
-//			pickup_list_container.style.display = 'block';
-//			pickup_list_container.style.right = "5%";
-//			pickup_list_container.style.top = "5%";
-//		});
-		switch(number_of_paragraph){
-			case 0:
-				add_more.style.left = "5%";
-				add_more.style.top = "29%";
-				break;
-			case 1:
-				add_more.style.left = "5%";
-				add_more.style.top = "53%";
-				break;
-			case 2:
-				add_more.style.left = "5%";
-				add_more.style.top = "77%";
-				break;
-			case 3:
-				add_more.style.left = "47%";
-				add_more.style.top = "5%";
-				break;
-			case 4:
-				add_more.style.left = "47%";
-				add_more.style.top = "29%";
-				break;
-			case 5:
-				add_more.style.left = "47%";
-				add_more.style.top = "53%";
-				break;
-			case 6:
-				add_more.style.left = "47%";
-				add_more.style.top = "77%";
-				break;
-		}
+	document.getElementById('new_paragraph_coment_'+number_of_paragraph).focus();
 
-		number_of_paragraph++;
-	}
-	if(number_of_paragraph == 8){
-		add_more.style.display = "none";
-	}
+	number_of_paragraph++;
 });
 
 add_more_old.addEventListener('click', () => {
 	if(counter_pickup_arr > 0){
 		pickup_list_container.style.display = 'block';
 		add_more_old.innerHTML = '가져온 일간 생각에,<p class="big_word">덧붙이기(' + (counter_pickup_arr-1) +  ')</p>'
+	} 
+})
+add_more_old.addEventListener('mouseenter', () => {
+	if(counter_pickup_arr == 0){
+		add_more_old.style.cursor = 'not-allowed';
 	}
 })
 
@@ -100,56 +68,53 @@ for(var i=0; i<pickup_list.length; i++){
 	(function(m){
 		pickup_arr[m].addEventListener('click', () => {
 			type_of_paragraph = 'pickup';
-			if(number_of_paragraph < 8){
-				tag_maker = document.createElement('pre');
-				tag_maker.setAttribute("class", "pickup_paragraph_content");
-				tag_maker.setAttribute("id", "pickup_paragraph_content_"+number_of_paragraph);
-				tag_maker.innerHTML = pickup_list[m][0].content;
-				writing_board.appendChild(tag_maker);
+			tag_maker = document.createElement('pre');
+			tag_maker.setAttribute("class", "pickup_paragraph_content");
+			tag_maker.setAttribute("id", "pickup_paragraph_content_"+number_of_paragraph);
+			tag_maker.innerHTML = pickup_list[m][0].content;
+			writing_board.appendChild(tag_maker);
 
-				tag_maker = document.createElement('pre');
-				tag_maker.setAttribute("class", "pickup_paragraph_title");
-				tag_maker.setAttribute("id", "pickup_paragraph_title_"+number_of_paragraph);
-				tag_maker.innerHTML = '☞ ' + pickup_list[m][0].question;
-				writing_board.appendChild(tag_maker);
+			tag_maker = document.createElement('pre');
+			tag_maker.setAttribute("class", "pickup_paragraph_title");
+			tag_maker.setAttribute("id", "pickup_paragraph_title_"+number_of_paragraph);
+			tag_maker.innerHTML = '☞ ' + pickup_list[m][0].question;
+			writing_board.appendChild(tag_maker);
 
-				tag_maker = document.createElement('pre');
-				tag_maker.setAttribute("class", "pickup_paragraph_date");
-				tag_maker.setAttribute("id", "pickup_paragraph_date_"+number_of_paragraph);
-				let date_ = pickup_list[m][0].created_date.split('T');
-				tag_maker.innerHTML = date_[0];
-				writing_board.appendChild(tag_maker);
+			tag_maker = document.createElement('pre');
+			tag_maker.setAttribute("class", "pickup_paragraph_date");
+			tag_maker.setAttribute("id", "pickup_paragraph_date_"+number_of_paragraph);
+			let date_ = pickup_list[m][0].created_date.split('T');
+			tag_maker.innerHTML = date_[0];
+			writing_board.appendChild(tag_maker);
 
-				tag_maker = document.createElement('textarea');
-				tag_maker.setAttribute("class", "pickup_paragraph_coment");
-				tag_maker.setAttribute("id", "pickup_paragraph_coment_"+number_of_paragraph);
-				tag_maker.setAttribute("onkeydown", "resize(this)");
-				tag_maker.setAttribute("name", "coments");
-				tag_maker.setAttribute("placeholder", "지금 읽어보기엔 어떤가요? 여전한가요, 달리 보이나요?");
-				writing_board.appendChild(tag_maker);
+			tag_maker = document.createElement('textarea');
+			tag_maker.setAttribute("class", "pickup_paragraph_coment");
+			tag_maker.setAttribute("id", "pickup_paragraph_coment_"+number_of_paragraph);
+			tag_maker.setAttribute("onkeydown", "resize(this)");
+			tag_maker.setAttribute("name", "coments");
+			tag_maker.setAttribute("placeholder", "지금 읽어보기엔 어떤가요? 여전한가요, 달리 보이나요?");
+			writing_board.appendChild(tag_maker);
 
-				tag_maker = document.createElement('input');
-				tag_maker.setAttribute("type", "hidden");
-				tag_maker.setAttribute("name", "titles");
-				tag_maker.setAttribute("value", pickup_list[m][0].question);
+			tag_maker = document.createElement('input');
+			tag_maker.setAttribute("type", "hidden");
+			tag_maker.setAttribute("name", "titles");
+			tag_maker.setAttribute("value", pickup_list[m][0].question);
+			writing_board.appendChild(tag_maker);
 
 
-				tag_maker = document.createElement('input');
-				tag_maker.setAttribute("type", "hidden");
-				tag_maker.setAttribute("name", "contents");
-				tag_maker.setAttribute("value", pickup_list[m][0].content);
+			tag_maker = document.createElement('input');
+			tag_maker.setAttribute("type", "hidden");
+			tag_maker.setAttribute("name", "contents");
+			tag_maker.setAttribute("value", pickup_list[m][0].content);
+			writing_board.appendChild(tag_maker);
 
-				document.getElementById('pickup_paragraph_coment_'+number_of_paragraph).focus();
+			document.getElementById('pickup_paragraph_coment_'+number_of_paragraph).focus();
 
-				pickup_arr[m].remove();
-				counter_pickup_arr--;
-				number_of_paragraph++;
-				pickup_list_container.style.display = 'none';
+			pickup_arr[m].remove();
+			counter_pickup_arr--;
+			number_of_paragraph++;
+			pickup_list_container.style.display = 'none';
 				
-			}
-			if(number_of_paragraph == 8){
-				add_more.style.display = "none";
-			}
 		});
 	})(i);
 }
