b27f101e88083ef070c63154a94127049011dabb
sy <greyhairchooselife@gmail.com>
2021-11-28 22:05:21 +0900

    유저피드백 라우터에 설문조사 영역 추가

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,6 +1,6 @@
 const db = require('../config/db.js').promise();
 
-const reported = (req, res) => {
+const bugs = (req, res) => {
 
 	const user_id = res.locals.user.id;
 	const body = req.body;
@@ -8,10 +8,21 @@ const reported = (req, res) => {
 	db.query(`INSERT INTO bug_report (user_id, page_id, screen_size, content, created_date) VALUES (?, ?, ?, ?, NOW())`, [user_id, body.page_id, body.screen_size, body.reporting_content]);
 
 	return res.redirect('/');
+}
 
+const betatest_survey = (req, res) => {
 
+	const user_id = res.locals.user.id;
+	const body = req.body;
+
+	db.query(`INSERT INTO betatest_survey (user_id, created_date, ) VALUES (?, NOW(), )`, [user_id, body.subject]);
+	
+	alert('1차 설문조사가 완료되었습니다.');
+
+	return res.redirect('/');
 }
 
 module.exports = {
-	reported,
+	bugs,
+	betatest_survey,
 };
--- routers/reportRouter.js
+++ routers/reportRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController.js');
 
-router.post('/', reportController.reported);
+router.post('/bugs', reportController.bugs);
+router.post('/betatest_survey', reportController.betatest_survey);
 
 module.exports = router;
